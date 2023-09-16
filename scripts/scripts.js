2be34ca77423063918d71f4ccbdd53ad86d382c4
ROMARIO3448 <romario344896@gmail.com>
2023-09-16 02:38:42 +0200

    Last for 15.09.2023 commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,3 +6,42 @@ function blinkingAnimationFunction() {
 }
 
 setTimeout(blinkingAnimationFunction, 500);
+
+//buttons implementation
+let calcScreenResultString = "";
+const arrayOfOperators = ["*", "/", "+", "-", "%", "."];
+const buttonsGridContainer = document.querySelector(".calc__buttons");
+const calcScreenResult = document.querySelector(".screen__result");
+const calcScreenInput = document.querySelector(".input__main");
+buttonsGridContainer.addEventListener("click", (event) => {
+    if (!event.target.classList.contains("button__item")) {
+        return;
+    }
+    if (event.target.classList.contains("button__item_unique-equal")) {
+        calcScreenResult.textContent = eval(calcScreenResultString);
+        return;
+    }
+    const temporaryConcatValue =
+        event.target.textContent.trim() === "÷"
+            ? "/"
+            : event.target.textContent.trim() === "x"
+            ? "*"
+            : event.target.textContent.trim() === ","
+            ? "."
+            : event.target.textContent.trim() === "C"
+            ? ""
+            : event.target.textContent.trim();
+    if (
+        arrayOfOperators.includes(temporaryConcatValue) &&
+        arrayOfOperators.includes(calcScreenResultString.slice(-1).toString())
+    ) {
+        return;
+    }
+    if (temporaryConcatValue) {
+        calcScreenResultString += temporaryConcatValue;
+    } else {
+        calcScreenResultString = "";
+    }
+    calcScreenInput.textContent = calcScreenResultString;
+    calcScreenResult.textContent = "";
+});
